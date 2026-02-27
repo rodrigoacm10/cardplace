74fc399a0beb69963a028a24c22d7a92fe23a0ff
Rodrigo Andrade <rodrigoacm10@gmail.com>
2026-02-26 22:58:38 -0300

    feat: correct header and content style

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import AppSidebar from '@/components/AppSidebar.vue'
 import { useRouter } from 'vue-router'
+import { LayoutDashboard } from 'lucide-vue-next'
 
 const router = useRouter()
 </script>
@@ -11,7 +12,7 @@ const router = useRouter()
     <AppSidebar />
 
     <main class="w-full flex flex-col min-h-screen text-white">
-      <header class="sticky top-0 z-10 bg-[#ffffff] flex justify-between items-center p-4">
+      <header class="sticky top-0 z-10 bg-[#ffffff] flex justify-between items-center py-4 px-8">
         <div class="flex items-center gap-4">
           <SidebarTrigger class="text-[#169366] hover:text-[#128159] cursor-pointer" />
           <div>
@@ -19,10 +20,30 @@ const router = useRouter()
           </div>
         </div>
 
-        <div></div>
+        <div class="text-[#4e4e4d] font-semibold flex items-center space-x-2">
+          <router-link to="/cards" class="flex items-center">
+            <LayoutDashboard class="w-4 h-4 mr-2" />
+            <span>Marketplace</span>
+          </router-link>
+
+          <div class="bg-[#4e4e4d] w-0.5 h-6"></div>
+
+          <router-link to="/cards" class="flex items-center">
+            <LayoutDashboard class="w-4 h-4 mr-2" />
+            <span>Marketplace</span>
+          </router-link>
+
+          <div class="bg-[#4e4e4d] w-0.5 h-6"></div>
+
+          <router-link to="/cards" class="flex items-center">
+            <LayoutDashboard class="w-4 h-4 mr-2" />
+            <span>Marketplace</span>
+          </router-link>
+        </div>
       </header>
 
-      <div class="p-8 flex-1 bg-[#169366]">
+      <!-- p-8 -->
+      <div class="flex-1 bg-[#eaeaea]">
         <div class="max-w-6xl mx-auto">
           <router-view />
         </div>
--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -3,19 +3,21 @@
 <template>
   <div class="min-h-screen text-white p-8">
     <div class="max-w-6xl mx-auto">
-      <header class="flex justify-between items-center mb-12">
+      <header class="flex justify-between items-center mb-8">
         <div>
-          <h1 class="text-3xl font-bold">Marketplace</h1>
-          <p class="text-zinc-400">Bem-vindo ao CardPlace</p>
+          <h1 class="text-[#128159] text-3xl font-bold">Marketplace</h1>
+          <p class="text-zinc-400">Veja todos os cards disponíveis para troca e compra</p>
         </div>
       </header>
 
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div
-          v-for="i in 6"
-          :key="i"
-          class="h-64 rounded-xl bg-zinc-900 border border-zinc-800 animate-pulse"
-        ></div>
+      <div class="">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div
+            v-for="i in 6"
+            :key="i"
+            class="h-64 rounded-xl bg-zinc-900 border border-zinc-800 animate-pulse"
+          ></div>
+        </div>
       </div>
     </div>
   </div>
